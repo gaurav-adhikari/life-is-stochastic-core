9c2b124bb7a45bdafd16ac501c87a4a5e2b58bb4
Gaurav Adhikari <gaurav.adhikari@pdc01.lisnepal.com.np>
2022-01-02 21:02:49 +0545

    (blog-welcome) added blog utilities classes

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import os
 
-@api_view(['GET'])
+from stochasticapi.utilities.api_response import BlogResponse
+
+
+@api_view(["GET"])
 def welcome_api(request):
-    return Response("Welcome to Blogs API")
+    return BlogResponse(message="Welcome to Blogs API v0.1").build_response()
---
+++ stochasticapi/utilities/http_codes.py
@@ -0,0 +1,11 @@
+from rest_framework import status
+
+
+OK = status.HTTP_200_OK
+
+CREATED = status.HTTP_201_CREATED
+
+UPDATED = status.HTTP_206_PARTIAL_CONTENT
+
+BAD_REQUEST = status.HTTP_400_BAD_REQUEST
+
