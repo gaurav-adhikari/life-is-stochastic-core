9d495ad6c606ba3144ade72bc953ab390ccfe693
Gaurav Adhikari <gaurav.adhikari@pdc01.lisnepal.com.np>
2022-01-06 16:15:00 +0545

    added cors fix

--- stochasticapi/settings.py
+++ stochasticapi/settings.py
@@ -49,12 +49,12 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 
